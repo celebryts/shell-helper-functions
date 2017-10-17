9cf1aefde6192ae35b069837b3442cb9dc154fad
Stavarengo <faelsta@gmail.com>
2017-10-17 14:46:21 -0200

    help functions

--- helper-functions.sh
+++ helper-functions.sh
@@ -78,10 +78,51 @@ function _waitForever {
     done
 }
 
+function _celyGetSecret {
+    key=$1
+    default=
+    if [ ${2+x} ]; then
+        default=$2
+    fi
+
+    if [ $(_getOverriteConfigFileContent CHECK_IF_SET $key) ]; then
+        printf "$(_getOverriteConfigFileContent env_value $key)"
+    elif [ $(_celyDoesSecretExists "$key") ]; then
+        printf "${CELY_SECRETS["$key"]}"
+    else
+        printf "$default"
+    fi
+}
+function _celyDoesSecretExists {
+    local key=$1
+    local ignoreOverrites=
+
+    if [ ! -z "$2" ]; then
+        ignoreOverrites=1
+    fi
+
+    if [[ ! $ignoreOverrites && $(_getOverriteConfigFileContent CHECK_IF_SET $key) ]]; then
+        echo 1
+    elif [[ ${CELY_SECRETS+x} && ${CELY_SECRETS[$key]+x} ]]; then
+        echo 1
+    else 
+        echo
+    fi
+}
+
 function _getOverriteConfigFileContent {
     local FORMAT=php
     if [ ! -z "$1" ]; then
         FORMAT=$1
+    fi
+
+    local LOKING_FOR_KEY=
+    if [[ "$FORMAT" == "env_value" || "$FORMAT" == "CHECK_IF_SET" ]]; then
+        if [ -z "$2" ]; then
+            echo "When format is \"$FORMAT\" you must provide a second argument with the key you are looking for."
+            return
+        fi
+        LOKING_FOR_KEY=$2
     fi
 
     local PHP_ARRAY_VAR_NAME=\$_DOCKER_CONTAINER_OVERRIDE_CONFIG
@@ -90,23 +131,41 @@ function _getOverriteConfigFileContent {
 
     local outputContent=""
     for OVERRIDE in $OVERRIDES; do
+        #printf "\n$OVERRIDE"
         local SHELL_ENV_NAME=$(echo $OVERRIDE | sed -r "s/=.+//")
+        local SHELL_ENV_NAME_WIHTOUT_PREFIX=$(echo $SHELL_ENV_NAME | sed -r "s/^$ENV_PREFIX//")
         local SHELL_ENV_VALUE=$(echo $OVERRIDE | sed -r "s/^.+?=//")
+        #printf "\n$SHELL_ENV_NAME_WIHTOUT_PREFIX"
 
         if [ "$FORMAT" == "php" ]; then
-            local line=$(echo $SHELL_ENV_NAME | sed -r "s/^$ENV_PREFIX//" | sed -r "s/,/']['/g")
+            local line=$(echo $SHELL_ENV_NAME_WIHTOUT_PREFIX | sed -r "s/,/']['/g")
             line="$PHP_ARRAY_VAR_NAME['$line']=$SHELL_ENV_VALUE;"
         elif [ "$FORMAT" == ".env" ]; then
-            local line=$(echo $SHELL_ENV_NAME | sed -r "s/^$ENV_PREFIX//")
+            local line=$SHELL_ENV_NAME_WIHTOUT_PREFIX
             line="$line=$SHELL_ENV_VALUE"
         elif [ "$FORMAT" == "bash_env" ]; then
-            local line=$(echo $SHELL_ENV_NAME | sed -r "s/^$ENV_PREFIX//")
+            local line=$SHELL_ENV_NAME_WIHTOUT_PREFIX
             line=$(echo $line | sed -r "s/[-,]/_/g")
             line="$line=$SHELL_ENV_VALUE"
+        elif [ "$SHELL_ENV_NAME_WIHTOUT_PREFIX" == "$LOKING_FOR_KEY" ]; then
+            if [ "$FORMAT" == "env_value" ]; then
+                printf "$SHELL_ENV_VALUE"
+                return
+            elif [ "$FORMAT" == "CHECK_IF_SET" ]; then
+                echo 1
+                return
+            fi
         fi
 
         outputContent="$outputContent\n$line"
     done
+
+    if [ "$FORMAT" == "env_value" ]; then
+        return
+    elif [ "$FORMAT" == "CHECK_IF_SET" ]; then
+        echo
+        return
+    fi
 
     if [ "$FORMAT" == "php" ]; then
         local outputTempFile=$(tempfile)
